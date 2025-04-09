59e05b9b546e0add130d0b6ec67fa8d3e1ca417b
DanieliCampos5 <dc525950@gmail.com>
2025-04-08 21:46:36 -0300

    Add nova atualização

--- requisicoes.php
+++ requisicoes.php
@@ -1,6 +1,15 @@
 <?php
+//Tratamento de Telefone
+function validarTelefone($telefone){
+    $padrao = '/^z(?\d{2}\)?\s?\d{4,5}-\d{4}$/ ';
+
+    return preg_match($padrao, $telefone);
+}
+
+// Tratamento nome da mãe
+
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    if(!empty($_POST['nome'])){
+    if(!empty($_POST['mae'])){
       echo"<p> Nome da mãe: <br> ". $_POST['mae']."!!";
     }
 }elseif($_SERVER["REQUEST_METHOD"] == "GET"){
@@ -11,14 +20,11 @@ if (preg_match('/[0-9]', $nome)){
     echo "Erro, o nome não pode conter números";
 }else{
     echo "Nome válido";
-    
+
 }
 
-function validarTelefone($telefone){
-    $padrao = '/^z(?\d{2}\)?\s?\d{4,5}-\d{4}$/ ';
 
-    return preg_match($padrao, $telefone);
-}
+
 
 
 
