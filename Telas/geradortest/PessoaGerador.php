e6599e09a95c616ee2da9b03ef13070dfd6a2d49
NobboliHD <fabiomartinsvini@gmail.com>
2025-04-22 21:38:21 -0300

    add
    PessoaGerador(com tudo que sera ussado no pdf gerador  ) tem todas as informações de todas as classes .

--- Telas/geradortest/PessoaGerador.php
+++ Telas/geradortest/PessoaGerador.php
@@ -10,5 +10,28 @@
         $this->admLiberacao = $admLiberacao;
         $this->exameLaboratorial = $exameLaboratorial;
     }
+    public function getPessoa(){
+        return $this->pessoa;
+    }
+    public function setPessoa(){
+
+        return $this->pessoa= $pessoa;
+    }
+    public function getExameLaboratorial(){
+        return $this->exameLaboratorial;
+    }
+    public function setExameLaboratorial($exameLaboratorial){
+
+        return $this->exameLaboratorial= $exameLaboratorial;
+    }
+    public function setAdmLiberacao($dmLiberacao){
+        return $this->admLiberacao=$admLiberacao;
+    }
+    public function getAdmLiberacao(){
+        return $this->admLiberacao;
+    }
+    public function __toString(){
+        return "pessoa:{$this->pessoa->__toString()} adm:{$this->admLiberacao->__toString()}";
+    }
  }
 ?>
