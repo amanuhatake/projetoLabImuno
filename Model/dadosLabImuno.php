c1125cc5936fb59aec5db5cfd48c9bbb5bebc30b
CalvinDusHub <calvinmateus@gmail.com>
2025-04-22 21:39:33 -0300

    classe dados lab imuno

---
+++ Model/dadosLabImuno.php
@@ -0,0 +1,48 @@
+<?php
+class DadosImuno{
+    private $nome;
+    private $registro;
+    private $lote;
+    private $validade;
+
+    public function getNome(){
+        return $this->nome;
+    }
+
+    public function setNome($nome){
+        $this->nome = $nome;
+    }
+
+    public function getRegistro(){
+        return $this->registro;
+    }
+
+    public function setRegistro($registro){
+        $this->registro = $registro;
+    }
+
+    public function getLote(){
+        return $this->lote;
+    }
+
+    public function setLote($lote){
+        $this->lote = $lote;
+    }
+
+    public function getValidade(){
+        return $this->validade;
+    }
+
+    public function setValidade($validade){
+        $this->validade = $validade;
+    }
+
+    //toString
+    public function __toString(){
+        return "Nome: {$this->nome}, Registro: {$this->registro}, Lote: {$this->lote}, Validade: {$this->validade}";
+    }
+
+}
+
+
+?>
