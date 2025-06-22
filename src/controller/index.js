81f285f440cc5ec08ed8f031e15446cb5c02ac6c
DanieliCampos5 <dc525950@gmail.com>
2025-06-22 19:34:06 -0300

    add(teste)

---
+++ src/controller/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+
+app.set("use engine", "ejs");
+app.set("views", "./src/views");
+
+app.get('/', (req, res) => {
+    res.send("Bem vindo ao sistema xpto");
+});
+
+
+//Dados pacientes
+//Recebendo os dados de Pacientes
+app.get("/pacientes", (req, res) =>    {
+
+    res.render("listarPaciente");
+})
+
+app.listen(3000, 'localhost', () => {
+    console.log("servidor rodando na porta 3000!");
+});
