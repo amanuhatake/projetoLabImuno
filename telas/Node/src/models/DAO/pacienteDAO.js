14dee2a4f9e8c2cd994e902111429ddaf3aa2c98
DanieliCampos5 <dc525950@gmail.com>
2025-06-23 18:59:29 -0300

    add(arrumacao)

--- telas/Node/src/models/DAO/pacienteDAO.js
+++ telas/Node/src/models/DAO/pacienteDAO.js
@@ -7,13 +7,13 @@ async function getPacientes(){
     return pac;
 }
 
-async function insertPaciente(nome, telefone, data, periodo, nomeMae, examesSolicitados, Email, Data_Nascimento, medicamento, medicamentoNome, patologia){
+async function insertPaciente(nome, telefone, Sexo, data, periodo, nomeMae, examesSolicitados, Email, Data_Nascimento, medicamento, medicamentoNome, patologia){
     if(nome, telefone, data, periodo, nomeMae, examesSolicitados, Email, Data_Nascimento, medicamento, medicamentoNome, patologia){
         const result = await pool.query(`
-            INSERT INTO pacientes(nome, telefone, data, periodo, nomeMae, examesSolicitados, Email, Data_Nascimento, medicamento, medicamentoNome, patologia)
-            values($1, $2, $3, $4, $5, $6, $7, $8,$9, $10, $11)
+            INSERT INTO pacientes(nome, telefone, Sexo, data, periodo, nomeMae, examesSolicitados, Email, Data_Nascimento, medicamento, medicamentoNome, patologia)
+            values($1, $2, $3, $4, $5, $6, $7, $8,$9, $10, $11, $12)
             RETURNING nome, telefone, data, periodo, nomeMae, examesSolicitados, Email, Data_Nascimento, medicamento, medicamentoNome, patologia`,
-            [nome, telefone, data, periodo, nomeMae, examesSolicitados, Email, Data_Nascimento, medicamento, medicamentoNome, patologia]
+            [nome, telefone,Sexo, data, periodo, nomeMae, examesSolicitados, Email, Data_Nascimento, medicamento, medicamentoNome, patologia]
         );  
        console.log("Resultado do insert:", result.rows[0]);
        if(result.rows.length > 0){
@@ -25,15 +25,15 @@ async function insertPaciente(nome, telefone, data, periodo, nomeMae, examesSoli
     return false;
 }    
 
-async function editPaciente(registro ,nome, telefone, data, periodo, nomeMae, examesSolicitados, Email, Data_Nascimento, medicamento, medicamentoNome, patologia){
+async function editPaciente(registro ,nome, telefone, Sexo, data, periodo, nomeMae, examesSolicitados, Email, Data_Nascimento, medicamento, medicamentoNome, patologia){
     if(registro, nome, telefone, data, periodo, nomeMae, examesSolicitados, Email, Data_Nascimento, medicamento, medicamentoNome, patologia){
         console.log("Dados: ", registro, nome, telefone, data, periodo, nomeMae, examesSolicitados, Email, Data_Nascimento, medicamento, medicamentoNome, patologia);
         const result = await pool.query(`
             UPDATE pacientes
-            SET nome = $1, telefone = $2, data = $3, periodo = $4, nomeMae = $5, examesSolicitados = $6, Email = $7, Data_Nascimento = $8, medicamento = $9, medicamentoNome = $10, patologia = $11
-            WHERE registro = $12;
-            RETURNING  nome, telefone, data, periodo, nomeMae, examesSolicitados, Email, Data_Nascimento, medicamento, medicamentoNome, patologia`,
-            [nome, telefone, data, periodo, nomeMae, examesSolicitados, Email, Data_Nascimento, medicamento, medicamentoNome, patologia]
+            SET nome = $1, telefone = $2, data = $3, periodo = $4, nomeMae = $5, examesSolicitados = $6, Email = $7, Data_Nascimento = $8, medicamento = $9, medicamentoNome = $10, patologia = $11, Sexo = $12
+            WHERE registro = $13;
+            RETURNING  nome, telefone, Sexo, data, periodo, nomeMae, examesSolicitados, Email, Data_Nascimento, medicamento, medicamentoNome, patologia`,
+            [nome, telefone, Sexo, data, periodo, nomeMae, examesSolicitados, Email, Data_Nascimento, medicamento, medicamentoNome, patologia]
         );
         console.log("Resultado do edit: " + result.rows[0]);
 
